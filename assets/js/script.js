8d71524f9a795d5710cb4e2e6ff59169698969ff
Britanic Cifuentes <brita1314@gmail.com>
2021-07-07 23:58:27 -0400

    ordenando y agregando comentarios

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Aquí hacemos funcionamiento de nuestro smoot scroll lento para nuestro menu
 $(document).ready(function(){
     $("a").on('click', function(event) {
         if (this.hash !== "") {
@@ -11,3 +12,17 @@ $(document).ready(function(){
         } 
     });
 });
+// Aquí hacemos funcionamiento de nuestro typeform
+$(function() { 
+    var qs,js,q,s,d=document, 
+    gi=d.getElementById, 
+    ce=d.createElement, 
+    gt=d.getElementsByTagName, 
+    id="typef_orm", 
+    b="https://embed.typeform.com/"; 
+    if(!gi.call(d,id)) { js=ce.call(d,"script"); 
+    js.id=id; js.src=b+"embed.js"; 
+    q=gt.call(d,"script")[0]; 
+    q.parentNode.insertBefore(js,q)
+    } 
+})

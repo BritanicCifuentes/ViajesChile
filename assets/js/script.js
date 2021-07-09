42454ac6af51d21734bf305f7e18e38d329a9d17
Britanic Cifuentes <brita1314@gmail.com>
2021-07-08 23:05:01 -0400

    Arreglando pequeñas cosas

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,14 +15,14 @@ $(document).ready(function(){
 // Aquí hacemos funcionamiento de nuestro typeform
 $(function() { 
     var qs,js,q,s,d=document, 
-    gi=d.getElementById, 
-    ce=d.createElement, 
-    gt=d.getElementsByTagName, 
-    id="typef_orm", 
-    b="https://embed.typeform.com/"; 
-    if(!gi.call(d,id)) { js=ce.call(d,"script"); 
-    js.id=id; js.src=b+"embed.js"; 
-    q=gt.call(d,"script")[0]; 
-    q.parentNode.insertBefore(js,q)
+        gi=d.getElementById, 
+        ce=d.createElement, 
+        gt=d.getElementsByTagName, 
+        id="typef_orm", 
+        b="https://embed.typeform.com/"; 
+        if(!gi.call(d,id)) { js=ce.call(d,"script"); 
+        js.id=id; js.src=b+"embed.js"; 
+        q=gt.call(d,"script")[0]; 
+        q.parentNode.insertBefore(js,q)
     } 
 })
